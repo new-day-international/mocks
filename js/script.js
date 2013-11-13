8241ac7101de2f9a414519c151a9ad64ef165b5a
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2013-11-13 12:04:25 -0500

    Adds working spaces nav hover dropdown with data

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function changeTheme(name) {
+  $('#bootstrap-theme').attr('href', "//netdna.bootstrapcdn.com/bootswatch/3.0.0/" + name + "/bootstrap.min.css");
+  $.cookie('theme', name);
+  $('body').removeClass('theme-' + $('body').data('theme'));
+  $('body').data('theme', name);
+  $('body').addClass('theme-' + name);
+  $('.theme').removeClass('disabled');
+  $('.theme a:contains("' + name + '")').parent().addClass('disabled');
+}
+
+// Run all jQuery
+$(function () {
+  $("time").timeago();
+
+  $(".theme").find('a').click(function(event) {
+    event.preventDefault();
+    changeTheme($(this).text());
+  })
+  var theme = $.cookie('theme') || 'cerulean'
+  changeTheme(theme);
+  if ($.cookie('instructions1') != 'hide') {
+    setTimeout(function() {
+      $('#instructions1').modal({});
+    }, 500);
+  }
+  $('#instructions1').on('hidden.bs.modal', function () {
+    $.cookie('instructions1', 'hide');
+  });
+
+});
